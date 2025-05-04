bed9012d1c1875863cd0a4d227c4e820f2dc54d1
LoSti <david.in.bergen@gmail.com>
2025-05-04 17:09:20 +0200

    Add .npmignore file and update Textcircle component options in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,17 @@
 <div class="container"></div>
 
 <div class="container">
-	<Textcircle text={['Svelte', 'Tailwind', 'Laravel', 'Typescript']}>
+	<Textcircle
+		text={['Svelte', 'Tailwind', 'Laravel', 'Typescript']}
+		options={{ dividerColor: '#f96743' }}
+	>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="80"
 			height="80"
 			viewBox="0 0 24 24"
 			fill="none"
-			stroke="currentColor"
+			stroke="#f96743"
 			stroke-width="2"
 			stroke-linecap="round"
 			stroke-linejoin="round"
